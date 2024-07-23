979783955c18568e809fd9e70ae8083d8a64bb75
Mahdi Sajedi <sajedi@usc.edu>
2024-07-23 01:34:09 +0000

    adding working cli

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
 """This is a cli"""
+
+import click, mylib, glob
+
+from mylib.myadd import add
+
+@click.command()
+@click.option('--value1', help='first number to add')
+@click.option('--value2', help='second number to add')
+
+def calculate(value1, value2):
+    """This adds 2 numbers"""
+    result = add(int(value1), int(value2))
+    click.echo(click.style(f"Adding numbers: {value1}, {value2}", fg="red"))
+    click.echo(click.style(f"Result: {result}", fg='blue'))
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    calculate()
